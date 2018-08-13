32c3950b82c949cd8bfa47b76f4fe43e70669069
shixiaowen <Laboratory@shixiaowen.com>
2018-08-13 16:33:51 +0800

    test whether it is exit

---
+++ src/com/changan/study/Test.java
@@ -0,0 +1,12 @@
+package com.changan.study;
+/*
+ * @Author Administrator-shixw.
+ * @Date 2018/8/13 0013.
+ *
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test the github!");
+    }
+}
